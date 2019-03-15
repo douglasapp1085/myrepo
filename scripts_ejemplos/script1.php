19855f0a5f71c020b41d7a8f9b4b6a2bf09b3ac7
douglasapp1085 <douglasparedes6@gmail.com>
2019-03-15 12:04:36 -0500

    php

---
+++ scripts_ejemplos/script1.php
@@ -0,0 +1,17 @@
+
+<center>programacion</center>
+<?php
+
+$nombre = "peter";
+$apellido = "watson";
+$edad = 30;
+$salario = 800;
+
+echo "<center>bienvenidos</center>";
+echo "nombre: ". $nombre . "<br>";
+echo "apellido: ". $apellido;
+echo "<br>edad: ". $edad;
+echo "<br>salario: ". $salario;
+
+ ?>
+<center>fin del programa</center>
---
+++ scripts_ejemplos/script2.php
@@ -0,0 +1,16 @@
+<?php
+$num1 = 10;
+$num2 = 5;
+$suma = $num1 + $num2;
+$resta = $num1 - $num2;
+$multiplicar = $num1 * $num2;
+$dividir = $num1 / $num2;
+
+echo "<center>";
+echo "calculadora";
+echo "<br>la suma es: ". $suma;
+echo "<br>la resta es: ". $resta;
+echo "<br>la multiplicacion es: ". $multiplicar;
+echo "<br>la divicion es: ". $dividir;
+echo "</center>";
+ ?>
